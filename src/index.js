3a85b39b73334ad86d5413f8360b3b291e42075b
koverboy <koverboy@mail.ru>
2019-03-22 22:29:53 +0300

    create solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let arr = [];
+
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+
+            if (str[i] === bracketsConfig[j][0]) {
+
+                if (bracketsConfig[j][0] === bracketsConfig[j][1]) {
+                    if (bracketsConfig[j][0] === arr[arr.length - 1]) {
+                        arr.pop();
+                    } else {
+                        arr.push(str[i]);
+                    }
+                } else {
+                    arr.push(str[i]);
+                }
+
+            } else if (str[i] === bracketsConfig[j][1]) {
+                if ((arr[arr.length - 1] === bracketsConfig[j][0])) {
+                    arr.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return arr.length ? false : true;
+};
